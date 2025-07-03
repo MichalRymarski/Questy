712324bf9499a55e376fc5f351b4f81650400e37
MichalRymarski <michal.rymarski2003@gmail.com>
2025-07-03 15:02:29 +0200

    fix : Add properties to test variant of application.properties
    Signed-off-by: MichalRymarski <michal.rymarski2003@gmail.com>

--- src/test/kotlin/questy/QuestyApplicationTests.kt
+++ src/test/kotlin/questy/QuestyApplicationTests.kt
@@ -2,10 +2,7 @@ package questy
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
-import org.testcontainers.utility.TestcontainersConfiguration
 
-@Import(TestcontainersConfiguration::class)
 @SpringBootTest
 class QuestyApplicationTests {
 
@@ -13,5 +10,4 @@ class QuestyApplicationTests {
     fun contextLoads() {
         assert(true)
     }
-
 }
